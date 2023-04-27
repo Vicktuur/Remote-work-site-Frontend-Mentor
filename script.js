6769311b665147685fa460a1e67f779e3607e282
Abiye Iniabere <104025786+Vicktuur@users.noreply.github.com>
2023-04-27 20:54:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// Selecting the Elements
+const hamburger = document.querySelector("#hamburger")
+const navigation = document.querySelector(".navigation")
+const body = document.querySelector(".body")
+const close = document.querySelector("#close")
+const arrow = document.querySelectorAll(".features")
+
+// activation of navigation bar
+hamburger.addEventListener("click", () => {
+    navigation.classList.toggle("active")
+
+    //setting delay on background color change
+    setTimeout(() => {
+        body.classList.add("active")
+    }, 500);
+})
+
+// deactivation of navigation bar
+close.addEventListener("click", () => {
+    navigation.classList.remove("active")
+    setTimeout(() => {
+        body.classList.remove("active")
+    }, 500)
+
+    //deactivates drop down menu when closing
+    arrow.forEach((lst) => {
+        close.addEventListener("click", () => {
+            lst.classList.remove("active")
+        })
+    })
+})
+
+body.addEventListener("click", () => {
+    navigation.classList.remove("active")
+    setTimeout(() => {
+        body.classList.remove("active")
+    }, 500) 
+})
+
+//drop down menu activation
+arrow.forEach((lst) => {
+    lst.addEventListener("click", () => {
+        lst.classList.toggle("active")
+    })
+    lst.addEventListener("mouseenter", () => {
+        lst.classList.add("active")
+    })
+    lst.addEventListener("mouseleave", () => {
+        lst.classList.remove("active")
+    })
+})
